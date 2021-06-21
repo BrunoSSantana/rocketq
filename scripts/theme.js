c4edde3c8c2cce3d0719d2714d647c1cea5410b4
BrunoSSantana <brunoosouza15@gmail.com>
2021-06-21 18:08:31 -0300

    first day

---
+++ scripts/theme.js
@@ -0,0 +1,42 @@
+const html = document.querySelector("html");
+const checkbox = document.querySelector("input[name=theme]");
+
+
+const getStyle = (element, style) => 
+    window
+        .getComputedStyle(element)
+        .getPropertyValue(style)
+
+const initialColors = {
+  background: '#F6FAFF',
+  primary: '#3485FF',
+  secondary: '#FAFAFA',
+  colorHead: '#0d114f',
+  greyPrimary: '#a1b2cd',
+  point: '#4F5'
+  
+}
+
+const rocketColors = {
+  background: '#454545',
+  primary: '#8257e5',
+  secondary: '#FAFAFA',
+  colorHead: '#FAFAFA',
+  greyPrimary: '#FAFAFA',
+  point:'#F6F6F6'
+}
+
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/g, "-$1").toLowerCase()
+
+
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => 
+        html.style.setProperty(transformKey(key), colors[key]) 
+    )
+}
+
+
+checkbox.addEventListener("change", ({target}) => {
+    target.checked ? changeColors(rocketColors) : changeColors(initialColors)
+})
