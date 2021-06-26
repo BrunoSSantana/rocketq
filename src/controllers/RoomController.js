2dafe1e35e79be1e34f9e2cc4b8d66359668e085
BrunoSSantana <brunoosouza15@gmail.com>
2021-06-26 12:28:55 -0300

    day 5  :art:

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,28 +1,61 @@
 const Database = require('../db/config')
 
 module.exports = {
-  async create(req, res){
+  async create(request, response){
     const db = await Database();
-    const pass = req.body.password;
+    const pass = request.body.password;
 
-    let roomId
+    let roomId;
+    let isRoom = true;
 
-    for(let i = 0; i < 6; i++){
-      i === 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-      roomId += Math.floor(Math.random() * 10).toString();
+    while (isRoom) {
+      
+      // Gera o númeor da sala
+      for(let i = 0; i < 6; i++){
+        i === 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+        roomId += Math.floor(Math.random() * 10).toString();
+      }
+  
+      // Verifica se o númerojá existi
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`);
+      // Retorna true par o primeiro id igual que encontrar
+      isRoom = roomsExistIds.some(roomsExistId => roomsExistId === roomId);
+      
+      if (!isRoom){
+      // Inseri o número da sala e a senha no banco
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+  
+        )`);
+      }
     }
-
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES (
-      ${parseInt(roomId)},
-      ${pass}
-
-    )`);
 
     await db.close();
 
-    res.redirect(`/room/${roomId}`);
+    response.redirect(`/room/${roomId}`);
+  },
+  async open(request, response){
+    const db = await Database();
+    const roomId = request.params.room;
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`);
+    const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+    let isNoQuestions
+
+    if(questions.length == 0){
+      if(questions.length == 0){
+        isNoQuestions = true
+      }
+    }
+
+    response.render('room', {roomId, questions, questionsRead, isNoQuestions})
+  },
+  enter(request, response){
+    const roomId = request.body.roomId;
+
+    response.redirect(`/room/${roomId}`)
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,11 @@ const RoomController = require('./controllers/RoomController');
 routes.get('/', (request, response) => response.render('index', {page: 'enter-room'} ));
 routes.get('/create-pass', (request, response) => response.render('index', {page: 'create-pass'}));
 
-routes.get('/room/:room', (request, response) => response.render('room'));
+routes.post('/create-room', RoomController.create);
+routes.get('/room/:room', RoomController.open);
+routes.post('/enterrom', RoomController.enter)
 
-// Formato que o formulário de dentro da modal tem que passar 
+routes.post('/question/create/:room', QuestionController.create)
 routes.post('/question/:room/:question/:action', QuestionController.index);
-routes.post('/create-room', RoomController.create)
 
 module.exports = routes;
